f974ea10d3f8d3f2b84fc9c3d4da92df8722c5b2
Steve <31158584+scc67@users.noreply.github.com>
2025-02-09 01:42:56 +0100

    after CH2 L5

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0))
+        
+        #pygame.Surface.fill(screen, (0,0,0))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
+        dt = clock.tick(60)
 
 
 if __name__ == "__main__":
